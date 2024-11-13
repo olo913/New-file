60d1b8fb4833349ab571cba4d10439452bf9d4b9
olo913 <oloruntoyin67@gmail.com>
2024-11-13 10:28:12 +0100

    list

---
+++ toy.py
@@ -0,0 +1,11 @@
+list = "Hellow world"
+print(list)
+# x = 0
+# list = ["apple", 10, "car", "paris"]
+# while len(list):
+    # print(list)
+
+# PSEUDO CODE
+# Creat a list of items
+# Creat a starting point for itration
+# While starting point is still less thanthe length of list
